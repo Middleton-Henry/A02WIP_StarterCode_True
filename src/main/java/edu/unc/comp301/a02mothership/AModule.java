40400446d45b0cef2f9bd535f910c6679dc0a73e
Middleton Henry <middhen@unc.edu>
2025-09-21 13:59:51 -0400

    fix: updated java version to match all files

--- src/main/java/edu/unc/comp301/a02mothership/AModule.java
+++ src/main/java/edu/unc/comp301/a02mothership/AModule.java
@@ -1,25 +1,22 @@
 package edu.unc.comp301.a02mothership;
 
-public abstract class AModule implements IModule{
-    private String name;
+public abstract class AModule implements IModule {
+  private String name;
 
-    AModule(String name){
-        this.name = name;
-    }
+  AModule(String name) {
+    this.name = name;
+  }
 
-    public String getName(){
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void statusReport(String moduleStatus, boolean isSuccessful){
-        System.out.println(name + " " + moduleStatus);
-        if(isSuccessful){
-            System.out.println("Action Successful.");
-        }
-        else{
-            System.out.println(name + " needs immediate attention!");
-        }
+  public void statusReport(String moduleStatus, boolean isSuccessful) {
+    System.out.println(name + " " + moduleStatus);
+    if (isSuccessful) {
+      System.out.println("Action Successful.");
+    } else {
+      System.out.println(name + " needs immediate attention!");
     }
+  }
 }
-
-
